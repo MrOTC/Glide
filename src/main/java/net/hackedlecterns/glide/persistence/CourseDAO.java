befec941e445063c0dde3efb658120b150435d25
sowgro <tpoke.ferrari@gmail.com>
2025-05-20 21:00:41 -0400

    Create CourseDAO

---
+++ src/main/java/net/hackedlecterns/glide/persistence/CourseDAO.java
@@ -0,0 +1,10 @@
+package net.hackedlecterns.glide.persistence;
+
+import net.hackedlecterns.glide.model.Course;
+
+import java.util.HashMap;
+
+public class CourseDAO {
+    // TODO this will be replaced with serialization eventually
+    public static HashMap<String, Course> courses = new HashMap();
+}
