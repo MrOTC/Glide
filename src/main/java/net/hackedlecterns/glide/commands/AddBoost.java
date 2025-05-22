88823cfb68c6366fe29aee40b86c557890de4a55
mrotc <newocart@gmail.com>
2025-05-21 22:00:08 -0400

    AddBoost handles enum from Boost

--- src/main/java/net/hackedlecterns/glide/commands/AddBoost.java
+++ src/main/java/net/hackedlecterns/glide/commands/AddBoost.java
@@ -6,10 +6,20 @@ import net.hackedlecterns.glide.util.CuboidRegion;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class AddBoost implements CommandExecutor {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static net.hackedlecterns.glide.commands.Pos1.pos1Map;
+import static net.hackedlecterns.glide.commands.Pos2.pos2Map;
+
+public class AddBoost implements CommandExecutor, TabCompleter {
     private static int boostCounter = 0;
 
     @Override
@@ -24,8 +34,8 @@ public class AddBoost implements CommandExecutor {
             return false;
         }
 
-        var pos1 = Pos1.pos1Map.get(player.getUniqueId());
-        var pos2 = Pos2.pos2Map.get(player.getUniqueId());
+        var pos1 = pos1Map.get(player.getUniqueId());
+        var pos2 = pos2Map.get(player.getUniqueId());
         if (pos1 == null || pos2 == null) {
             player.sendMessage("Please set both /pos1 and /pos2 first.");
             return false;
@@ -37,7 +47,14 @@ public class AddBoost implements CommandExecutor {
         }
 
         double speed = Double.parseDouble(args[0]);
-        Boost.BoostDirection direction = Boost.BoostDirection.valueOf(args[1].toUpperCase());
+        Boost.BoostDirection direction;
+        try {
+            direction = Boost.BoostDirection.valueOf(args[1].toUpperCase());
+        } catch (IllegalArgumentException e) {
+            sender.sendMessage("Invalid direction. Valid directions: " + Arrays.toString(Boost.BoostDirection.values()));
+            return false;
+        }
+
         String name = (args.length >= 3) ? args[2] : "boost_" + (++boostCounter);
 
         Course course = EditCourse.getEditingCourse(player);
@@ -47,5 +64,14 @@ public class AddBoost implements CommandExecutor {
 
         player.sendMessage("Boost added.");
         return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if (args.length == 2) {
+            return Arrays.stream(Boost.BoostDirection.values())
+                    .map(Enum::name).collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 }
