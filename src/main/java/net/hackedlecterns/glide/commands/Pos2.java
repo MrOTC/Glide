57c10b279dfe04628d89b0d65feceb810e2f2def
mrotc <newocart@gmail.com>
2025-05-21 20:37:10 -0400

    Pos1 and Pos2 Commands

---
+++ src/main/java/net/hackedlecterns/glide/commands/Pos2.java
@@ -0,0 +1,28 @@
+package net.hackedlecterns.glide.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Pos2 implements CommandExecutor {
+    public static final Map<UUID, Location> pos2Map = new HashMap<>();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("Only players can use this command.");
+            return false;
+        }
+
+        pos2Map.put(player.getUniqueId(), player.getLocation());
+        player.sendMessage("Position 2 set.");
+        return true;
+    }
+}
